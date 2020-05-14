71585ada093cae7d28cc1181bebe07395fa09094
Ryota Seki <ryotabs@hawaii.edu>
2020-05-14 11:14:45 -1000

    Remove modules that are not used

--- config.h
+++ config.h
@@ -3,15 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
-	{"",	"torrent",	20,	7},
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	13}, */
 	/* {"",	"moonphase",	18000,	5}, */
 	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
 	/* {"",	"nettraf",	1,	16}, */
 	{"",	"volume",	0,	10},
 	{"",	"battery | tr \'\n\' \' \'",	5,	3},
